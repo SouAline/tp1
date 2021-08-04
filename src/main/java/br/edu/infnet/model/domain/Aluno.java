65f4b3c1b2f0fc6f7274707518e0fb4fb0839084
Aline Souza <ds.alinesouza@gmail.com>
2021-08-04 18:59:54 -0300

    Atualiza classe Aluno
    adiciona atributos mensalidade e idade

--- src/main/java/br/edu/infnet/model/domain/Aluno.java
+++ src/main/java/br/edu/infnet/model/domain/Aluno.java
@@ -3,28 +3,32 @@ package br.edu.infnet.model.domain;
 public class Aluno {
 
 	private String nome;
+	private int idade;
+	private float mensalidade;
 	private String email;
 	private String curso;
 	private String regiao;
 	private String[] disciplinas;
-	
-	
+
 	public Aluno(String nome, String email) {
-		this.nome= nome;
-		this.email= email;
+		this.nome = nome;
+		this.email = email;
 	}
-	
+
 	@Override
 	public String toString() {
-		
-		return String.format("[%s] o aluno %s (%s) da região %s está inscrito em %d disciplinas!\n", curso, nome, email, regiao, disciplinas.length);
-		
+
+		return String.format(
+				"[%s] o aluno %s, com idade de %d cadastrado no e-mail %s da região %s está inscrito em %d disciplinas e paga %f de mensalidade!\n",
+				curso, nome, idade, email, regiao, disciplinas.length, mensalidade);
+
 	}
+
 	public void impressao() {
 		System.out.println(this);
 
-		System.out.println("Disciplinas:");		
-		for(String disc : disciplinas) {
+		System.out.println("Disciplinas:");
+		for (String disc : disciplinas) {
 			System.out.println("- " + disc);
 		}
 	}
@@ -35,6 +39,24 @@ public class Aluno {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+
+	public float getMensalidade() {
+		return mensalidade;
+	}
+
+	public void setMensalidade(float mensalidade) {
+		this.mensalidade = mensalidade;
 	}
 
 	public String getEmail() {
@@ -68,6 +90,5 @@ public class Aluno {
 	public void setDisciplinas(String[] disciplinas) {
 		this.disciplinas = disciplinas;
 	}
-	
-	
+
 }
